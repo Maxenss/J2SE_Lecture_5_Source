7b0dd7f5b08b67f4358521baf5b0903a640f3750
Maxenss <maxenssss@gmail.com>
2016-11-15 00:55:22 +0200

    Initial commit

---
+++ src/com/easylabs/Book.java
@@ -0,0 +1,15 @@
+package com.easylabs;
+
+/**
+ * Created by Maxim on 15.11.2016.
+ */
+public class Book {
+
+    public Book(String _name, String _autorName){
+        Name = _name;
+        AuthorName = _autorName;
+    }
+
+    public String Name;
+    public String AuthorName;
+}
